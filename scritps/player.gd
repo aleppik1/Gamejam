5dd8a737ed5cba6f355761f5524fae1a4efef990
itosjats <nyguz3@gmail.com>
2024-03-29 18:07:53 +0200

    some basic screen restriction

--- scritps/player.gd
+++ scritps/player.gd
@@ -2,10 +2,22 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
+	
 	move_and_slide()
+	var screen_size = get_viewport_rect().size
+	print(global_position.y)
+	if global_position.x > 1280:
+		global_position.x = 1280;
+	elif global_position.x < 0:
+		global_position.x = 0;
+	elif global_position.y < 40:
+		global_position.y = 40;
+	elif global_position.y > 2000:
+		global_position.y = 2000;
